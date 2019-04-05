7f78a0da1a624d132d2d09b06cb982576fa3772e
Peadar O Cathain <peadarocathain@gmail.com>
2019-04-06 00:00:31 +0100

    SpecemMean Draft 1

---
+++ SpecmMean.py
@@ -0,0 +1,25 @@
+#This program will compare the mean of each species with the
+#mean of the total dataset
+#Peadar O Cathain
+
+
+import numpy as ny
+
+#Read the date file into an array
+data = ny.genfromtxt('Iris dataset.csv', delimiter=',')
+
+#sort the data set by species (use while true statement??)
+
+
+#col1 = data[1:,0]
+#col2 = data[2:,0]
+#print (firstcol) testing the correct col returned
+meancol1 = ny.mean(data[1:,0])
+meancol2 = ny.mean(data[1:,1])
+meancol3 = ny.mean(data[1:,2])
+meancol4 = ny.mean(data[1:,3])
+
+print("The Mean of the first column is: ", meancol1,
+"The Mean of the second column is: ", meancol2,
+"The Mean of the third column is: ", meancol3,
+"The Mean of the fourth column is: ", meancol4,)
