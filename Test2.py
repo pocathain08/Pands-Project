3b626370f05aaa3932ae436fdc26706cd44e1a51
Peadar O Cathain <peadarocathain@gmail.com>
2019-04-08 21:25:13 +0100

    Test2 Draft 2

--- Test2.py
+++ Test2.py
@@ -17,18 +17,8 @@ sns.set(style="white", color_codes=True)
 #However no need to give full directory as file and program are in the same directory
 iris = pd.read_csv("Iris dataset.csv") # the iris dataset is now a Pandas DataFrame
 
+
 # Let's see what's in the iris data - Jupyter notebooks print the result of the last thing you do
-##head=iris.head()
-##print (head) #Prints the first 5 rows
-##Ref https://shapeofdata.wordpress.com/2013/10/01/case-study-1-iris/
-colors = ['red','green','blue'] # Dictionary classes -> colors
-data = numpy.array(data, dtype="float") # Convert data to an array
-M = (data-numpy.mean(data.T, axis=1)).T # Mean-center the data
+head=iris.head()
+print (head) #Prints the first 5 rows
 
-# Find the matrix of principal components
-[latent, coeff] = numpy.linalg.eig(numpy.cov(M)) 
-coords = numpy.dot(coeff.T, M)   # Convert data to PC coordinates
-
-fig, ax = matplotlib.pyplot.subplots() # Create matplotlib figure
-for i in range(data.shape[0]): # Plot all the points
-    ax.scatter(coords[0,i], coords[1,i], color=colors[labels[i]]
