3848b3e9dd177934f64e5e9446991bfcf953f7d5
Peadar O Cathain <peadarocathain@gmail.com>
2019-04-09 21:14:08 +0100

    Test2 Draft 3

--- Test2.py
+++ Test2.py
@@ -1,24 +1,34 @@
 #This notebook demos Python data visualizations on the Iris dataset
-# Ref :https://www.kaggle.com/benhamner/python-data-visualizations
+# Ref :https://www.kaggle.com/jchen2186/machine-learning-with-iris-dataset
 #peadar o cathain 08 Apr 2019
 
-#Import pandas, a data processing and CSV file I/O library
 import pandas as pd
-import numpy as ny
-
-#Import seaborn, a Python graphing library
-import warnings # current version of seaborn generates a bunch of warnings that we'll ignore
-warnings.filterwarnings("ignore")
-import seaborn as sns
+import numpy as np
 import matplotlib.pyplot as plt
-sns.set(style="white", color_codes=True)
+import matplotlib
+import seaborn as sns
+sns.set(color_codes=True)
 
-# Load the Iris flower dataset, from C:\Users\35386\OneDrive\Desktop\Pands-Project\Iris dataset.csv.
-#However no need to give full directory as file and program are in the same directory
-iris = pd.read_csv("Iris dataset.csv") # the iris dataset is now a Pandas DataFrame
+#%matplotlib inline
+#%pylab inline
+#matplotlib inline
+
+from sklearn import metrics
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+
+data = pd.read_csv('Iris dataset.csv')
+
+print (data.head())
+print (data.tail())
+ 
+print (data.info())
+print (data.describe())
+
+#print (data['Species'].value_counts())
 
 
-# Let's see what's in the iris data - Jupyter notebooks print the result of the last thing you do
-head=iris.head()
-print (head) #Prints the first 5 rows
+
+
 
