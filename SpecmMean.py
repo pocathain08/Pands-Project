8108d9a5c6195ea036004a6c04626fbbb8507df0
Peadar O Cathain <peadarocathain@gmail.com>
2019-04-10 14:05:52 +0100

    SpecmMean D2

--- SpecmMean.py
+++ SpecmMean.py
@@ -15,3 +15,13 @@ print (data.head())
 #col1 = data[1:,0]
 #col2 = data[2:,0]
 #print (firstcol) testing the correct col returned
+or (data[1:,4]) == ('setosa'):
+    meancol1 = ny.mean(data[1:,0])
+    meancol2 = ny.mean(data[1:,1])
+    meancol3 = ny.mean(data[1:,2])
+    meancol4 = ny.mean(data[1:,3])
+
+print("The Mean of the first column is: ", meancol1,
+"The Mean of the second column is: ", meancol2,
+"The Mean of the third column is: ", meancol3,
+"The Mean of the fourth column is: ", meancol4,)
