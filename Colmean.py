feda27ff83a5ef3fa63925f85644261a69b17e69
Peadar O Cathain <peadarocathain@gmail.com>
2019-05-02 21:32:56 +0100

    pandas input

--- Colmean.py
+++ Colmean.py
@@ -6,7 +6,6 @@ import numpy as ny
 #Read the date file into an array
 data = ny.genfromtxt('Iris dataset.csv', delimiter=',')
 
-
 #col1 = data[1:,0]
 #col2 = data[2:,0]
 #print (firstcol) testing the correct col returned
@@ -19,3 +18,19 @@ print("The Mean of the first column is: ", meancol1,
 "The Mean of the second column is: ", meancol2,
 "The Mean of the third column is: ", meancol3,
 "The Mean of the fourth column is: ", meancol4,)
+
+import pandas as pd
+
+data = pd.read_csv("Iris dataset.csv")
+
+print (type(data))
+#Number of rows and columns 
+print(data.shape)
+#Number of dimensions in dataframe
+print(data.ndim)
+#The types of each column
+print(data.dtypes)
+#Description of the data, basic stats on cols
+print(data.describe())
+
+
